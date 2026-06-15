a9963b84e69e6c698e8a29ec4966ae27ced7e2c4
shota <shota.brokishvili@gmail.com>
2026-06-15 04:31:14 -0700

    calculate temperature

---
+++ FitnessApp/Program.cs
@@ -0,0 +1,5 @@
+﻿using FitnessApp.Services;
+using Homework.Helpers;
+
+SharedUtils.InitializeConsole();
+TemperatureCalculator.returnCorrectTemperature();
---
+++ FitnessApp/Services/TemperatureCalculator.cs
@@ -0,0 +1,46 @@
+﻿using Homework.Helpers;
+
+namespace FitnessApp.Services;
+
+public static class TemperatureCalculator
+{
+
+    public static void returnCorrectTemperature()
+    {
+        Console.WriteLine("შეიყვანეთ რომელ გრადუსზე გსურთ ტემპერატურის არჩევა");
+        string temperatureInCelsius = Console.ReadLine();
+        
+        Console.WriteLine("შეიყვანეთ სასურველი ტემპერატურა ციფრებში");
+        int intInput = int.Parse(Console.ReadLine());
+
+        if (temperatureInCelsius == "Celsius")
+        {
+            CelsiusToFahrenheit(intInput);
+        }
+        else if (temperatureInCelsius == "Fahrenheit")
+        {
+            FahrenheitToCelsius(intInput);
+        }
+        else
+        {
+            Console.WriteLine("გთხოვთ შეიყვანოთ სწორი ტემპერატურის ერთეული");
+        }
+        
+    }
+    
+    
+    public static void CelsiusToFahrenheit(int intInput)
+    {
+            int Fahrenheit = (intInput * 9 / 5) + 32 ;
+            Console.WriteLine($"თქვენი ტემპერატურა ფარენჰეიტშია: {Fahrenheit}");
+
+    }
+
+    public static void FahrenheitToCelsius(int intInput)
+    { 
+            int celsius = (intInput - 32) * 5 / 9;
+            Console.WriteLine($"თქვენი ტემპერატურა ცელსიუსში: {celsius}");
+    }
+   
+    
+}
