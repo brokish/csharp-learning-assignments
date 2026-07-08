a43d01a095dd065eef6716ff3ce5f41bf943d41f
shota <shota.brokishvili@gmail.com>
2026-07-08 08:06:25 -0700

    assigment 9

---
+++ Assigment9_library/Program.cs
@@ -0,0 +1,17 @@
+﻿using Assigment9_library.MainProgram;
+using Homework.Helpers;
+using Microsoft.VisualBasic.CompilerServices;
+
+SharedUtils.InitializeConsole();
+
+MainLogics mainLogics = new MainLogics();
+
+mainLogics.studentAppRunner();
+
+// mainLogics.searchStudents();
+
+// mainLogics.BookDictionary();
+
+// mainLogics.TupleAppRunner();
+
+
